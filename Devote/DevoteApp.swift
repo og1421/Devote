c5dad07d3e6ef80a87c068435aa143d239086008
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-10 19:50:46 -0300

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Orlando Moraes Martins on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
