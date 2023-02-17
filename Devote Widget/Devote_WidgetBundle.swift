6b8a337c04747402c4a33529206a3739bbc0f62e
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-17 20:29:00 -0300

    Creating Widget

---
+++ Devote Widget/Devote_WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Devote_WidgetBundle.swift
+//  Devote Widget
+//
+//  Created by Orlando Moraes Martins on 17/02/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Devote_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Devote_Widget()
+        Devote_WidgetLiveActivity()
+    }
+}
